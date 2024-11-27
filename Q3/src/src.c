6d1244c6ee5b0042156477a0ea6cd687430d1df2
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2024-11-27 10:03:31 -0300

    Removendo rendundancias

--- Q3/src/src.c
+++ Q3/src/src.c
@@ -13,30 +13,31 @@ Info *CreateInfo(int start, int end, int status)
         info->end = end;
         info->status = status;
     }
-    return (info);
+    return info;
 }
 
 Memory *createNode(Info *information, Memory *leftChild, Memory *centerChild)
 {
     Memory *node = (Memory *)malloc(sizeof(Memory));
 
-    if (!node)
-        printf("Falha ao criar o Nó!\n");
-
-    if (node)
+    if (node != NULL)
     {
         node->info1 = information;
         node->left = leftChild;
         node->center = centerChild;
         node->numKeys = 1;
     }
+    else
+    {
+        printf("Falha ao criar o Nó!\n");
+    }
 
-    return (node);
+    return node;
 }
 
 int isLeaf(Memory *node)
 {
-    return (node->left == NULL);
+    return node->left == NULL;
 }
 
 void AddInfo(Memory **node, Info *info, Memory *child)
@@ -61,24 +62,18 @@ void AddInfo(Memory **node, Info *info, Memory *child)
     {
         Memory *newNode = (Memory *)malloc(sizeof(Memory));
         newNode->numKeys = 1;
-        newNode->left = NULL;
-        newNode->center = NULL;
-        newNode->right = NULL;
 
         if (info->start > (*node)->info2->start)
         {
             newNode->info1 = info;
             newNode->left = (*node)->right;
             newNode->center = child;
-            (*node)->numKeys = 1;
         }
-        else if (info->start > (*node)->info1->start && info->start < (*node)->info2->start)
+        else if (info->start > (*node)->info1->start)
         {
             newNode->info1 = (*node)->info2;
             newNode->left = child;
             newNode->center = (*node)->right;
-            (*node)->info2 = NULL;
-            (*node)->numKeys = 1;
         }
         else
         {
@@ -88,12 +83,8 @@ void AddInfo(Memory **node, Info *info, Memory *child)
             newNode->left = (*node)->center;
             newNode->center = (*node)->right;
             (*node)->center = child;
-            (*node)->numKeys = 1;
         }
-
-        // Split splitResult;
-        // splitResult.largestNode = newNode;
-        // splitResult.promote = (*node)->info2;
+        (*node)->numKeys = 1;
     }
 }
 
@@ -103,18 +94,11 @@ Split SplitNode(Memory **root, Info *info, Memory *child)
     Info *promote = NULL;
     Split itBroke;
 
-    // Inicializando o novo nó para armazenar a chave promovida
-    largestNode->info1 = NULL;
-    largestNode->info2 = NULL;
-    largestNode->left = NULL;
-    largestNode->center = NULL;
-    largestNode->right = NULL;
     largestNode->numKeys = 1;
 
     if (info->start > (*root)->info2->start)
     {
         promote = (*root)->info2;
-        (*root)->info2 = NULL;
         (*root)->numKeys = 1;
 
         largestNode->info1 = info;
@@ -128,7 +112,6 @@ Split SplitNode(Memory **root, Info *info, Memory *child)
         largestNode->left = child;
         largestNode->center = (*root)->right;
 
-        (*root)->info2 = NULL;
         (*root)->numKeys = 1;
     }
     else
@@ -140,17 +123,13 @@ Split SplitNode(Memory **root, Info *info, Memory *child)
         largestNode->left = (*root)->center;
         largestNode->center = (*root)->right;
 
-        (*root)->info2 = NULL;
         (*root)->numKeys = 1;
     }
-
-    (*root)->center = NULL;
-    (*root)->right = NULL;
 
     itBroke.largestNode = largestNode;
     itBroke.promote = promote;
 
-    return (itBroke);
+    return itBroke;
 }
 
 void Insert23(Memory **root, Memory *parent, Info **promote, int start, int end, int status, int *flag)
@@ -161,51 +140,43 @@ void Insert23(Memory **root, Memory *parent, Info **promote, int start, int end,
         *root = createNode(nova_info, NULL, NULL);
         *flag = 1;
     }
-    else
+    else if (isLeaf(*root))
     {
-        if (isLeaf(*root))
+        *flag = 1;
+        if ((*root)->numKeys < 2)
         {
-            *flag = 1;
-            if ((*root)->numKeys < 2)
-            {
-                Info *nova_info = CreateInfo(start, end, status);
-                AddInfo(root, nova_info, NULL);
-            }
-            else
-            {
-                Info *nova_info = CreateInfo(start, end, status);
-                Split resultado = SplitNode(root, nova_info, NULL);
-
-                if (!parent)
-                    *root = createNode(resultado.promote, *root, resultado.largestNode);
-
-                else
-                {
-                    if (parent->numKeys < 2)
-                        AddInfo(&parent, resultado.promote, resultado.largestNode);
-                    else
-                    {
-                        resultado = SplitNode(&parent, resultado.promote, resultado.largestNode);
-                        parent = createNode(resultado.promote, parent, resultado.largestNode);
-                    }
-                }
-            }
+            Info *nova_info = CreateInfo(start, end, status);
+            AddInfo(root, nova_info, NULL);
         }
         else
         {
-            if (start < (*root)->info1->start)
-                Insert23(&(*root)->left, *root, promote, start, end, status, flag);
-            else if ((*root)->numKeys == 1 || (start < (*root)->info2->start))
-                Insert23(&(*root)->center, *root, promote, start, end, status, flag);
+            Info *nova_info = CreateInfo(start, end, status);
+            Split resultado = SplitNode(root, nova_info, NULL);
+
+            if (!parent)
+                *root = createNode(resultado.promote, *root, resultado.largestNode);
+            else if (parent->numKeys < 2)
+                AddInfo(&parent, resultado.promote, resultado.largestNode);
             else
-                Insert23(&(*root)->right, *root, promote, start, end, status, flag);
+            {
+                resultado = SplitNode(&parent, resultado.promote, resultado.largestNode);
+                parent = createNode(resultado.promote, parent, resultado.largestNode);
+            }
         }
+    }
+    else
+    {
+        if (start < (*root)->info1->start)
+            Insert23(&(*root)->left, *root, promote, start, end, status, flag);
+        else if ((*root)->numKeys == 1 || start < (*root)->info2->start)
+            Insert23(&(*root)->center, *root, promote, start, end, status, flag);
+        else
+            Insert23(&(*root)->right, *root, promote, start, end, status, flag);
     }
 }
 
 Memory *FindSpace(Memory *root, int requiredSpace)
 {
-    Memory *found = NULL;
     if (root != NULL)
     {
         if (isLeaf(root))
@@ -214,20 +185,19 @@ Memory *FindSpace(Memory *root, int requiredSpace)
             int espacoDisponivel2 = (root->numKeys == 2) ? root->info2->end - root->info2->start : 0;
 
             if (root->info1->status == FREE && espacoDisponivel1 >= requiredSpace)
-                found = root;
-            else if (root->numKeys == 2 && root->info2->status == FREE && espacoDisponivel2 >= requiredSpace)
-                found = root;
+                return root;
+            if (root->numKeys == 2 && root->info2->status == FREE && espacoDisponivel2 >= requiredSpace)
+                return root;
         }
         else
         {
-            found = FindSpace(root->left, requiredSpace);
-            if (found == NULL)
-                found = FindSpace(root->center, requiredSpace);
-            if (found == NULL && root->numKeys == 2)
-                found = FindSpace(root->right, requiredSpace);
+            Memory *found = FindSpace(root->left, requiredSpace);
+            if (!found) found = FindSpace(root->center, requiredSpace);
+            if (!found && root->numKeys == 2) found = FindSpace(root->right, requiredSpace);
+            return found;
         }
     }
-    return (found);
+    return NULL;
 }
 
 Memory *SourceSpace(Memory *root, int requiredSpace)
@@ -250,4 +220,3 @@ void DisplayInfos(Memory *root)
         }
     }
 }
-
