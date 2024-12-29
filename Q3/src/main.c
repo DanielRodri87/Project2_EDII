4ae9af70e5c6d99301e365b96ef51efc68affc46
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2024-12-29 11:00:41 -0300

    CHORE: Concatena de forma automática

--- Q3/src/main.c
+++ Q3/src/main.c
@@ -3,6 +3,41 @@
 #include <time.h>
 #include "src.h"
 
+int findFirstStart(Memory *root)
+{
+    int result = -1;
+
+    if (root != NULL)
+    {
+        Memory *current = root;
+
+        while (current->left != NULL)
+            current = current->left;
+        result = current->info1->start;
+    }
+
+    return result;
+}
+
+int findLastStart(Memory *root)
+{
+    int result = -1;
+
+    if (root != NULL)
+    {
+        Memory *current = root;
+        while (current->right != NULL)
+            current = current->right;
+
+        if (current->numKeys == 2 && current->info2 != NULL)
+            result = current->info2->start;
+        else
+            result = current->info1->start;
+    }
+
+    return result;
+}
+
 void DisplayMenu()
 {
     printf("\n===============================================================\n");
@@ -36,8 +71,9 @@ int main()
     Memory *tree = NULL;
     Info *newInfo;
     Info promove;
-    int start = 0, end = 0, status = 2, requiredSpace, control_menu = 0, sub_menu = 0, bloco;
+    int start = 0, end = 0, status = 2, requiredSpace, control_menu = 0, sub_menu = 0, bloco, space, start2 = 0;
     char opc;
+
     srand(time(NULL));
 
     // Solicitar o tamanho da memória
@@ -111,7 +147,28 @@ int main()
         case 1:
             printf("\nInforme o espaço necessário para alocação: ");
             scanf("%d", &requiredSpace);
-            AllocateSpace(&tree, requiredSpace);
+            space = AllocateSpace(&tree, requiredSpace, &start2);
+            if (space == requiredSpace)
+            {
+                if (findFirstStart(tree) == start2)
+                {
+                    int remover_inicio, remover_fim;
+                    remover_fim = mergeNodesStart(&tree, &remover_inicio);
+                    newInfo = CreateInfo(remover_inicio, remover_fim, 0);
+                    removeFromMemory(NULL, &tree, newInfo);
+                    printf("Compactação realizada no início da memória.\n");
+                }
+                else if (findLastStart(tree) == start2)
+                {
+                    int remover_inicio_fim, remover_fim_fim;
+                    remover_fim_fim = mergeNodesEnd(&tree, &remover_inicio_fim);
+                    newInfo = CreateInfo(remover_inicio_fim, remover_fim_fim, 0);
+                    removeFromMemory(NULL, &tree, newInfo);
+                    printf("Compactação realizada no fim da memória.\n");
+                }
+                else
+                    printf("Aqui é a compactação do centro que não funciona\n");
+            }
             break;
 
         case 2:
@@ -129,40 +186,40 @@ int main()
             switch (sub_menu)
             {
             case 1:
-                {
-                    int remover_inicio, remover_fim;
-                    remover_fim = mergeNodesStart(&tree, &remover_inicio);
-                    newInfo = CreateInfo(remover_inicio, remover_fim, 0);
-                    removeFromMemory(NULL, &tree, newInfo);
-                    printf("Compactação realizada no início da memória.\n");
-                }
-                break;
+            {
+                int remover_inicio, remover_fim;
+                remover_fim = mergeNodesStart(&tree, &remover_inicio);
+                newInfo = CreateInfo(remover_inicio, remover_fim, 0);
+                removeFromMemory(NULL, &tree, newInfo);
+                printf("Compactação realizada no início da memória.\n");
+            }
+            break;
 
             case 2:
-                {
-                    int remover_inicio_meio1, remover_fim_meio1;
-                    int remover_inicio_meio2, remover_fim_meio2;
-                    mergeNodesMiddle(&tree, &remover_inicio_meio1, &remover_fim_meio1, &remover_inicio_meio2, &remover_fim_meio2);
+            {
+                int remover_inicio_meio1, remover_fim_meio1;
+                int remover_inicio_meio2, remover_fim_meio2;
+                mergeNodesMiddle(&tree, &remover_inicio_meio1, &remover_fim_meio1, &remover_inicio_meio2, &remover_fim_meio2);
 
-                    newInfo = CreateInfo(remover_inicio_meio1, remover_fim_meio1, 0);
-                    removeFromMemory(&tree, &tree, newInfo);
+                newInfo = CreateInfo(remover_inicio_meio1, remover_fim_meio1, 0);
+                removeFromMemory(&tree, &tree, newInfo);
 
-                    newInfo = CreateInfo(remover_inicio_meio2, remover_fim_meio2, 0);
-                    removeFromMemory(&tree, &tree, newInfo);
+                newInfo = CreateInfo(remover_inicio_meio2, remover_fim_meio2, 0);
+                removeFromMemory(&tree, &tree, newInfo);
 
-                    printf("Compactação realizada no meio da memória.\n");
-                }
-                break;
+                printf("Compactação realizada no meio da memória.\n");
+            }
+            break;
 
             case 3:
-                {
-                    int remover_inicio_fim, remover_fim_fim;
-                    remover_fim_fim = mergeNodesEnd(&tree, &remover_inicio_fim);
-                    newInfo = CreateInfo(remover_inicio_fim, remover_fim_fim, 0);
-                    removeFromMemory(NULL, &tree, newInfo);
-                    printf("Compactação realizada no fim da memória.\n");
-                }
-                break;
+            {
+                int remover_inicio_fim, remover_fim_fim;
+                remover_fim_fim = mergeNodesEnd(&tree, &remover_inicio_fim);
+                newInfo = CreateInfo(remover_inicio_fim, remover_fim_fim, 0);
+                removeFromMemory(NULL, &tree, newInfo);
+                printf("Compactação realizada no fim da memória.\n");
+            }
+            break;
 
             case 4:
                 printf("\nVoltando ao menu principal...\n");
